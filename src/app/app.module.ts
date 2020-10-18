b1e95976595409d7f3c0a43070d235e022c5cf40
Tosin17 <kajopelayeola@gmail.com>
2020-10-17 20:44:54 -0400

    Initialize tab component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconInputModule } from './lib/icon-input.module';
+import { TabComponent } from './lib/tab/tab.component';
+import { TabPanelComponent } from './lib/tab-panel/tab-panel.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TabPanelComponent, TabComponent],
   imports: [BrowserModule, AppRoutingModule, IconInputModule],
   providers: [],
   bootstrap: [AppComponent],
