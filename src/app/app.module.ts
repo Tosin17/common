0cfc29edd12cf1f9d0b447f18c8e14669d52fc58
Tosin17 <kajopelayeola@gmail.com>
2020-11-08 13:29:59 -0500

    Initialize modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { IconInputModule } from './lib/icon-input.module';
 import { TabComponent } from './lib/tab/tab.component';
 import { TabPanelComponent } from './lib/tab-panel/tab-panel.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [AppComponent, TabPanelComponent, TabComponent],
+  declarations: [AppComponent, TabPanelComponent, TabComponent, ModalComponent],
   imports: [BrowserModule, AppRoutingModule, IconInputModule],
   providers: [],
   bootstrap: [AppComponent],
