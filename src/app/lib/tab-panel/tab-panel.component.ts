3fb631a004ba1a83f2a9f544a68d9e19c618e731
Tosin17 <kajopelayeola@gmail.com>
2020-11-08 13:04:17 -0500

    Add alternative/projected tab headers

--- src/app/lib/tab-panel/tab-panel.component.ts
+++ src/app/lib/tab-panel/tab-panel.component.ts
@@ -2,8 +2,10 @@ import {
   AfterContentInit,
   Component,
   ContentChildren,
+  Input,
   OnInit,
   QueryList,
+  TemplateRef,
 } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
@@ -12,18 +14,20 @@ import { TabComponent } from '../tab/tab.component';
   templateUrl: './tab-panel.component.html',
   styleUrls: ['./tab-panel.component.scss'],
 })
-export class TabPanelComponent implements OnInit, AfterContentInit {
+export class TabPanelComponent implements AfterContentInit {
+  @Input() headerTmpl: TemplateRef<any>;
   @ContentChildren(TabComponent)
   tabs: QueryList<TabComponent>;
-
-  constructor() {}
-
-  ngOnInit(): void {}
+  tabsContext = {
+    tabs: null,
+  };
 
   ngAfterContentInit() {
-    if (!this.tabs.find((tab) => tab.selected)) {
+    if (this.tabs.first) {
       this.tabs.first.selected = true;
     }
+
+    this.tabsContext.tabs = this.tabs;
   }
 
   setSelected(tab: TabComponent) {
