4da6aef567c0b4d16c64bc1e0d1bc6e6927f6df3
Tosin17 <kajopelayeola@gmail.com>
2020-11-23 03:06:55 -0500

    Initialize masked inputs markup

---
+++ src/app/lib/masked-input/masked-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-masked-input',
+  templateUrl: './masked-input.component.html',
+  styleUrls: ['./masked-input.component.scss']
+})
+export class MaskedInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
